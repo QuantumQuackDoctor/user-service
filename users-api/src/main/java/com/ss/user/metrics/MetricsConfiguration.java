d4efd308db80d14332e9e49effd9a1582f7212b9
Ezra Mitchell <ezra.mitchell@smoothstack.com>
2021-11-24 17:53:22 -0700

    commiting to save progress

---
+++ users-api/src/main/java/com/ss/user/metrics/MetricsConfiguration.java
@@ -0,0 +1,45 @@
+package com.ss.user.metrics;
+
+import io.micrometer.cloudwatch2.CloudWatchConfig;
+import io.micrometer.cloudwatch2.CloudWatchMeterRegistry;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class MetricsConfiguration {
+    @Bean
+    public CloudWatchAsyncClient cloudWatchAsyncClient() {
+        return CloudWatchAsyncClient.builder().build();
+    }
+
+    @Bean
+    public MeterRegistry getMeterRegistry(CloudWatchConfig cloudWatchConfig, CloudWatchAsyncClient cloudWatchAsyncClient) {
+        return new CloudWatchMeterRegistry(
+                cloudWatchConfig,
+                Clock.SYSTEM,
+                cloudWatchAsyncClient
+        );
+    }
+
+    @Bean
+    public CloudWatchConfig setupCloudWatchConfig(@Value("${metrics.cloudwatch.namespace}")String namespace){
+        return new CloudWatchConfig() {
+            private final Map<String, String> configuration = new HashMap<String, String>() {{
+                put("cloudwatch.namespace", namespace);
+                put("cloudwatch.step", Duration.ofMinutes(1).toString());
+            }};
+            @Override
+            public String get(String s) {
+                return configuration.get(s);
+            }
+        };
+    }
+}
